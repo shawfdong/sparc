f2a445b4882939a6f555ac1aaec5d22e5692f359
Lizhen Shi <lizhen9.shi@gmail.com>
2017-06-25 05:07:22 +0000

    set checkpoint path

--- scripts/run_cc.sh
+++ scripts/run_cc.sh
@@ -5,7 +5,7 @@ WAIT=1
  
 CMD=`cat<<EOF
 $SPARK_SUBMIT --master $MASTER --deploy-mode client --driver-memory 55G --driver-cores 5 --executor-memory 20G  --executor-cores 2 --conf spark.default.parallelism=54 --conf spark.driver.maxResultSize=5g --conf spark.network.timeout=360000 --conf spark.eventLog.enabled=$ENABLE_LOG $TARGET \
-GraphCC --wait $WAIT  -i $INPUT  -o $OUTPUT -n 162 --min_shared_kmers $min_shared_kmers  --max_shared_kmers $max_shared_kmers --min_reads_per_cluster $min_reads_per_cluster --n_iteration 1
+GraphCC --wait $WAIT  -i $INPUT  -o $OUTPUT -n 162 --min_shared_kmers $min_shared_kmers  --max_shared_kmers $max_shared_kmers --min_reads_per_cluster $min_reads_per_cluster --n_iteration 1 --use_graphframes
 EOF`
 
 echo $CMD
