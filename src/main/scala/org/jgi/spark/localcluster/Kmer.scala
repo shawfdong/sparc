70219a87828a4f52a3608387a68c935ff904646a
Lizhen Shi <lizhen9.shi@gmail.com>
2017-06-28 15:53:21 -0400

    experimental code

--- src/main/scala/org/jgi/spark/localcluster/Kmer.scala
+++ src/main/scala/org/jgi/spark/localcluster/Kmer.scala
@@ -6,16 +6,19 @@ package org.jgi.spark.localcluster
 object Kmer {
 
   private def canonical_kmer(seq: String) = {
-    val rc =DNASeq.reverse_complement(seq)
+    val rc = DNASeq.reverse_complement(seq)
     if (rc < seq) rc else seq
   }
 
   // generates kmers hash values given the seq (doesn't handle 'N' and reverse complement)
   private def _generate_kmer(seq: String, k: Int) = {
     (0 until (seq.length - k + 1)).map {
-      i => DNASeq.from_bases(canonical_kmer(seq.substring(i, i + k)))
+      i =>
+        val subseq = seq.substring(i, i + k)
+        DNASeq.from_bases(canonical_kmer(subseq))
     }
   }
+
 
   // generates kmers
   def generate_kmer(seq: String, k: Int): Array[DNASeq] = {
@@ -24,5 +27,29 @@ object Kmer {
         _generate_kmer(subSeq, k)
       }
     }.distinct
+  }
+
+  def main(args: Array[String]) = {
+    implicit class Crossable[X](xs: Traversable[X]) {
+      def cross[Y](ys: Traversable[Y]) = for {x <- xs; y <- ys} yield (x, y)
+    }
+
+    def f(a1: Seq[String]): String = {
+      val a = a1.map(u => "\"" + u + "\"")
+      println(a.size)
+      s"Array(${a.mkString(",")})"
+    }
+
+    val a = Seq("A", "T", "C", "G")
+    println(f(a))
+
+    val b = a.cross(a).map(u => u.productIterator.toSeq.map(_.asInstanceOf[String]).mkString).toSeq.distinct
+    println(f(b))
+
+    val c = a.cross(b).map(u => u.productIterator.toSeq.map(_.asInstanceOf[String]).mkString).toSeq.distinct
+    println(f(c))
+
+    val d = a.cross(c).map(u => u.productIterator.toSeq.map(_.asInstanceOf[String]).mkString).toSeq.distinct
+    println(f(d))
   }
 }
---
+++ src/test/scala/org/jgi/spark/localcluster/tools/KmerCounting2Spec.scala
@@ -0,0 +1,62 @@
+package org.jgi.spark.localcluster.tools
+
+import com.holdenkarau.spark.testing.SharedSparkContext
+import org.apache.spark.SparkConf
+import org.scalatest.{FlatSpec, Matchers}
+import sext._
+
+/**
+  * Created by Lizhen Shi on 5/14/17.
+  */
+class KmerCounting2Spec extends FlatSpec with Matchers  with SharedSparkContext
+{
+
+  "parse command line" should "be good" in {
+    val cfg = KmerCounting2.parse_command_line("-i test -p *.seq -o tmp".split(" ")).get
+    cfg.input should be("test")
+  }
+  "kmer counting" should "work on the test seq files" in {
+    val cfg = KmerCounting2.parse_command_line("-i test/small -k 31 -p sample.seq -o tmp/kmercounting2_seq_test.txt --n_iteration 1".split(" ")).get
+    print(s"called with arguments\n${cfg.valueTreeString}")
+    KmerCounting2.run(cfg, sc)
+  }
+
+  "kmer counting" should "work on the test seq files with canonical_kmer" in {
+    val cfg = KmerCounting2.parse_command_line("-i test/small -k 31 -C -p sample.seq -o tmp/kmercounting2_seq_test_C.txt --n_iteration 1".split(" ")).get
+    print(s"called with arguments\n${cfg.valueTreeString}")
+    KmerCounting2.run(cfg, sc)
+  }
+
+  "kmer counting" should "work on the test seq files with multiple N" in {
+    val cfg = KmerCounting2.parse_command_line("-i test/small -k 31 -p sample2.seq -o tmp/kmercounting2_seq_sample2.txt --n_iteration 1 -k 15".split(" ")).get
+    print(s"called with arguments\n${cfg.valueTreeString}")
+    KmerCounting2.run(cfg, sc)
+  }
+
+
+  "kmer counting" should "work on the test base64 files" in {
+    val cfg = KmerCounting2.parse_command_line("-i test/small -k 31 -p sample.base64 --format base64 -o tmp/kmercounting2_base64_test.txt --n_iteration 4".split(" ")).get
+    print(s"called with arguments\n${cfg.valueTreeString}")
+    KmerCounting2.run(cfg, sc)
+  }
+
+  "kmer counting" should "work on the test base64 files with canonical_kmer" in {
+    val cfg = KmerCounting2.parse_command_line("-i test/small -k 31 -p sample.base64 --format base64 -o tmp/kmercounting2_base64_test_C.txt --n_iteration 4 -C".split(" ")).get
+    print(s"called with arguments\n${cfg.valueTreeString}")
+    KmerCounting2.run(cfg, sc)
+  }
+
+  "kmer counting" should "work on the test base64 files with multiple N" in {
+    val cfg = KmerCounting2.parse_command_line("-i test/small -k 31 -p sample2.base64 --format base64 -o tmp/kmercounting2_base64_sample2.txt --n_iteration 2 -k 15".split(" ")).get
+    print(s"called with arguments\n${cfg.valueTreeString}")
+    KmerCounting2.run(cfg, sc)
+  }
+
+  //*************************************** fake
+  "kmer counting" should "work on fake seq file" in {
+    val cfg = KmerCounting2.parse_command_line("-i test/small -k 31 -p fake_sample.seq -o tmp/kmercounting2_seq_fake.txt --n_iteration 1".split(" ")).get
+    print(s"called with arguments\n${cfg.valueTreeString}")
+    KmerCounting2.run(cfg, sc)
+  }
+
+}
